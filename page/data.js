b95fab56e44e300865a9d9cf395d862906af2c74
kongxiangrun <xiangrun.kong@grandhonor.net>
2023-05-22 17:58:52 +0800

    af

--- page/data.js
+++ page/data.js
@@ -1,9 +1,77 @@
-export default {
-  pageUrls: [
-    {
-      classification: '高重',
-      name: 'JIRA',
-      url: 'http://192.169.4.13:8088/secure/Dashboard.jspa'
-    }
-  ]
-}
+export default [
+  {
+
+    classification: '高重',
+    name: 'Nacos',
+    url:"http://192.168.103.201:8868/nacos",
+    color:'#252a2f'
+  },
+
+  {
+
+    classification: '高重',
+    name: 'Jenkins',
+    url:"http://192.168.103.203:8765/",
+    color:'#000000'
+  },
+
+  {
+    classification: '高重',
+    name: 'GitLab',
+    url: 'http://192.169.4.143/',
+    color:'#292961'
+  },
+  {
+    classification: '高重',
+    name: 'JIRA',
+    url: 'http://192.169.4.13:8088/secure/Dashboard.jspa',
+    color:'#0747a6'
+  },
+
+
+  {
+    classification: '高重',
+    name: '远程会见',
+    url: 'http://192.168.103.201:7309/',
+  },
+  {
+    classification: '高重',
+    name: '智慧城市',
+    url: 'http://192.168.103.201:9002/user/login'
+  },
+  {
+    classification: '高重',
+    name: 'ChatGpt',
+    url: 'https://192.168.103.201/'
+  },
+  {
+    classification: '高重',
+    name: '看守所考核',
+    url: 'http://192.168.103.201:6802/'
+  }
+  ,
+  {
+    classification: '高重',
+    name: '狱警考核',
+    url: 'http://192.168.103.202:6902/'
+  },
+  {
+    classification: '高重',
+    name: '东航',
+    url: 'http://192.168.103.201:7609/emmc/'
+  },
+
+
+  {
+    classification: '高重',
+    name: 'Aiot',
+    url: 'http://192.168.103.201:7209/user/login'
+  },
+
+
+
+
+
+
+
+]
