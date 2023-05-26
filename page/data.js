906e1ce638ba14bd686f2e0bb4c7f49f5add6dc8
kongxiangrun <xiangrun.kong@grandhonor.net>
2023-05-26 17:01:28 +0800

    a

--- page/data.js
+++ page/data.js
@@ -1,7 +1,7 @@
 export default [
   {
 
-    classification: '高重',
+    classification: '服务',
     name: 'Nacos',
     url:"http://192.168.103.201:8868/nacos",
     color:'#252a2f'
@@ -9,20 +9,20 @@ export default [
 
   {
 
-    classification: '高重',
+    classification: '服务',
     name: 'Jenkins',
     url:"http://192.168.103.203:8765/",
     color:'#000000'
   },
 
   {
-    classification: '高重',
+    classification: '服务',
     name: 'GitLab',
     url: 'http://192.169.4.143/',
     color:'#292961'
   },
   {
-    classification: '高重',
+    classification: '服务',
     name: 'JIRA',
     url: 'http://192.169.4.13:8088/secure/Dashboard.jspa',
     color:'#0747a6'
@@ -30,47 +30,47 @@ export default [
 
 
   {
-    classification: '高重',
+    classification: '系统',
     name: '远程会见',
     url: 'http://192.168.103.201:7309/',
   },
   {
-    classification: '高重',
+    classification: '系统',
     name: '智慧城市',
     url: 'http://192.168.103.201:9002/user/login'
   },
   {
-    classification: '高重',
+    classification: '服务',
     name: 'ChatGpt',
     url: 'https://192.168.103.201/'
   },
   {
-    classification: '高重',
+    classification: '系统',
     name: '看守所考核',
     url: 'http://192.168.103.201:6802/'
   }
   ,
   {
-    classification: '高重',
+    classification: '系统',
     name: '狱警考核',
     url: 'http://192.168.103.202:6902/'
   },
   {
-    classification: '高重',
+    classification: '系统',
     name: '东航',
     url: 'http://192.168.103.201:7609/emmc/'
   },
 
 
   {
-    classification: '高重',
+    classification: '系统',
     name: 'Aiot',
     url: 'http://192.168.103.201:7209/user/login'
   },
 
 
   {
-    classification: '高重',
+    classification: '原型',
     name:'狱警考核',
     url:'https://modao.cc/app/7Jxc6qHrsiy7wqE0Tbmuv#screen=slfmbkbgy4ti31o'
   },
